9a5d0c59044365880164ca63116844cde8ade505
Javier de Silóniz Sandino <javier.s.s@47deg.com>
2015-01-27 17:12:11 +0100

    Added basic methods to store and load to disk parsed data from the servers

--- ScalaDays/ScalaDaysTests/ScalaDaysTests.swift
+++ ScalaDays/ScalaDaysTests/ScalaDaysTests.swift
@@ -14,6 +14,16 @@ class ScalaDaysTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        let documentsFolderPath = StoringHelper.documentsFolderPath()
+        let fileManager = NSFileManager.defaultManager()
+        
+        if(!fileManager.fileExistsAtPath(documentsFolderPath)) {
+            var error : NSErrorPointer = nil
+            if !fileManager.createDirectoryAtPath(documentsFolderPath, withIntermediateDirectories: true, attributes: nil, error: error) {
+                XCTFail("Couldn't create fake documents directory for testing...")
+            }
+        }        
     }
     
     override func tearDown() {
@@ -33,4 +43,26 @@ class ScalaDaysTests: XCTestCase {
         }
     }
     
+    func testStoringAndLoadingConference() {
+        var information = Information(id: 111, name: "scaladays-sanfran-2015", longName: "Scala Days San Francisco", nameAndLocation: "Scala Days San Francisco, March 16-18, San Francisco, California", firstDay: "2015-03-16", lastDay: "2015-03-20", normalSite: "http://gotocon.com/scaladays-sanfran-2015", registrationSite: "https://secure.trifork.com/scaladays-sanfran-2015/registration/", utcTimezoneOffset: "America/Los_Angeles", utcTimezoneOffsetMillis: -25200000)
+        var conference = Conference(info: information, schedule: [], sponsors: [], speakers: [])
+        StoringHelper.sharedInstance.storeConferenceData(conference)
+        let loadedData = StoringHelper.sharedInstance.loadConferenceData()
+        if let loadedConference = loadedData {
+            // MARK: Testing equality of both conference instances...
+            XCTAssert(conference.info.name == loadedConference.info.name, "Conference data should be the same after being stored: field name")
+            XCTAssert(conference.info.id == loadedConference.info.id, "Conference data should be the same after being stored: field id")
+            XCTAssert(conference.info.nameAndLocation == loadedConference.info.nameAndLocation, "Conference data should be the same after being stored: field nameAndLocation")
+            XCTAssert(conference.info.firstDay == loadedConference.info.firstDay, "Conference data should be the same after being stored: field firstDay")
+            XCTAssert(conference.info.lastDay == loadedConference.info.lastDay, "Conference data should be the same after being stored: field lastDay")
+            XCTAssert(conference.info.normalSite == loadedConference.info.normalSite, "Conference data should be the same after being stored: field normalSite")
+            XCTAssert(conference.info.registrationSite == loadedConference.info.registrationSite, "Conference data should be the same after being stored: field registrationSite")
+            XCTAssert(conference.info.utcTimezoneOffset == loadedConference.info.utcTimezoneOffset, "Conference data should be the same after being stored: field utcTimezoneOffset")
+            XCTAssert(conference.info.utcTimezoneOffsetMillis == loadedConference.info.utcTimezoneOffsetMillis, "Conference data should be the same after being stored: field utcTimezoneOffsetMillis")
+            
+            // TODO: missing the rest of the fields...
+        } else {
+            XCTFail("Couldn't load valid conference data from disk")
+        }
+    }
 }
