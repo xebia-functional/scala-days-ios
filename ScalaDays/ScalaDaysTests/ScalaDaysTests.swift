639f8740ed7dc3a7fdd280f1dd0a5b9b27f6f49c
Rafa Paradela <rafa.p@47deg.com>
2015-02-03 12:33:39 +0100

    Edited test to conference accepts a new param: codeOfConduct.

--- ScalaDays/ScalaDaysTests/ScalaDaysTests.swift
+++ ScalaDays/ScalaDaysTests/ScalaDaysTests.swift
@@ -49,10 +49,59 @@ class ScalaDaysTests: XCTestCase {
     }
     
     func testStoringAndLoadingConference() {
-        var information = Information(id: 111, name: "scaladays-sanfran-2015", longName: "Scala Days San Francisco", nameAndLocation: "Scala Days San Francisco, March 16-18, San Francisco, California", firstDay: "2015-03-16", lastDay: "2015-03-20", normalSite: "http://gotocon.com/scaladays-sanfran-2015", registrationSite: "https://secure.trifork.com/scaladays-sanfran-2015/registration/", utcTimezoneOffset: "America/Los_Angeles", utcTimezoneOffsetMillis: -25200000)
-        var speaker = Speaker(bio: "Speaker 1 biography\nhttp://event.scaladays.org", company: "Company", id: 1111, name: "Nice Guy 1", picture: "", title: "", twitter: "@speaker")
-        var event = Event(id: 6520, title: "Registration Open", apiDescription: "", type: 3, startTime: "2015-03-16T23:00:00Z", endTime: "2015-03-16T23:00:00Z", date: "MONDAY MARCH 16", track: Track(id: 1051, name: "Keynote", host: "", shortdescription: "", apiDescription: ""), location: Location(id: 589, name: "Herbst Pavilion", mapUrl: ""), speakers: [speaker])
-        var conference = Conference(info: information, schedule: [event], sponsors: [SponsorType(type: "Hosted by", items: [Sponsor(logo: "http://event.scaladays.org/dl/photos/sponsors/sponsor1.png", url: "http://www.scala-days-sponsor1.com")])], speakers: [speaker])
+        var information = Information(
+            id: 111,
+            name: "scaladays-sanfran-2015",
+            longName: "Scala Days San Francisco",
+            nameAndLocation: "Scala Days San Francisco, March 16-18, San Francisco, California",
+            firstDay: "2015-03-16",
+            lastDay: "2015-03-20",
+            normalSite: "http://gotocon.com/scaladays-sanfran-2015",
+            registrationSite: "https://secure.trifork.com/scaladays-sanfran-2015/registration/",
+            utcTimezoneOffset: "America/Los_Angeles",
+            utcTimezoneOffsetMillis: -25200000)
+        
+        var speaker = Speaker(
+            bio: "Speaker 1 biography\nhttp://event.scaladays.org",
+            company: "Company",
+            id: 1111,
+            name: "Nice Guy 1",
+            picture: "",
+            title: "",
+            twitter: "@speaker")
+        
+        var event = Event(
+            id: 6520,
+            title: "Registration Open",
+            apiDescription: "",
+            type: 3,
+            startTime: "2015-03-16T23:00:00Z",
+            endTime: "2015-03-16T23:00:00Z",
+            date: "MONDAY MARCH 16",
+            track: Track(
+                id: 1051,
+                name: "Keynote",
+                host: "",
+                shortdescription: "",
+                apiDescription: ""),
+            location: Location(
+                id: 589,
+                name: "Herbst Pavilion",
+                mapUrl: ""),
+            speakers: [speaker])
+        
+        var conference = Conference(
+            info: information,
+            schedule: [event],
+            sponsors: [SponsorType(
+                type: "Hosted by",
+                items: [Sponsor(
+                    logo: "http://event.scaladays.org/dl/photos/sponsors/sponsor1.png",
+                    url: "http://www.scala-days-sponsor1.com")]
+                )],
+            speakers: [speaker],
+            codeOfConduct: "Our Code of Conduct is inspired by the kind folks at NE Scala, who adopted theirs from PNW Scala.")
+        
         StoringHelper.sharedInstance.storeConferenceData(conference)
         let loadedData = StoringHelper.sharedInstance.loadConferenceData()
         if let loadedConference = loadedData {
