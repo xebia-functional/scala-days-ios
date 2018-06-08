b17d535d0d92fcc150953efb12f6621cbdad1b2c
Juan Cazalla Estrella <juan.cazalla@47deg.com>
2018-06-08 09:57:51 +0200

    Fixes header comment

--- ScalaDays/ScalaDaysTests/VoteTests.swift
+++ ScalaDays/ScalaDaysTests/VoteTests.swift
@@ -1,10 +1,18 @@
-//
-//  VoteTests.swift
-//  ScalaDaysTests
-//
-//  Created by Juan Cazalla Estrella on 16/05/2018.
-//  Copyright © 2018 47 Degrees. All rights reserved.
-//
+/*
+ * Copyright (C) 2018 47 Degrees, LLC http://47deg.com hello@47deg.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import XCTest
 
@@ -30,6 +38,5 @@ class VoteTests: XCTestCase {
         XCTAssertEqual(storedVotes["key"]?.talkId, loadedVotes["key"]?.talkId)
         XCTAssertEqual(storedVotes["key"]?.conferenceId, loadedVotes["key"]?.conferenceId)
         XCTAssertEqual(storedVotes["key"]?.comments, loadedVotes["key"]?.comments)
-
     }
 }
