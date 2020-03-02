b01616d9ac4c0a6aace13201eb18fcd590e63715
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-03-02 15:03:26 +0100

    Update ScalaDays/Extension/Array+Extension.swift
    Co-Authored-By: Tomás Ruiz-López <truizlop@gmail.com>

--- ScalaDays/Extension/Array+Extension.swift
+++ ScalaDays/Extension/Array+Extension.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Array {
     
     public subscript(safe index: Int) -> Element? {
-        guard index < count else { return nil }
+        guard index >= 0 && index < count else { return nil }
         return self[index]
     }
 }
